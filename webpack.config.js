38ff1d02b544ff189a834505bfa9f034922c57b4
xApnea <jcbruno150@gmail.com>
2020-06-17 17:27:13 -0400

    Webpack is configured and bundling

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './client/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [{
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ['@babel/preset-react'],
+        }
+      }
+    ]
+  }
+};
