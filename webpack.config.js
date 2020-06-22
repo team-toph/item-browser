8bed6fc84960f4f1a7b72f474056baef25328d8b
xApnea <jcbruno150@gmail.com>
2020-06-22 17:20:58 -0400

    Added babel plugin for styled-components, ease of debugging

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
         loader: "babel-loader",
         options: {
           presets: ['@babel/preset-react'],
+          plugins: ['babel-plugin-styled-components']
         }
       }
     ]
