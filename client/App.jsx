2194f613e0012a5de9c102fc010d3be004396403
xApnea <jcbruno150@gmail.com>
2020-06-23 16:10:59 -0400

    Added title, subtitle info w/ stars, description

--- client/App.jsx
+++ client/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 import Carousel from './Carousel.jsx';
+import Title from './Title.jsx';
+import StyleSelector from './StyleSelector.jsx';
 
 class App extends React.Component {
   constructor() {
@@ -8,6 +10,7 @@ class App extends React.Component {
     this.state = {
       product: {},
       variations: [],
+      currentVariant: {},
       currentVariantsImages: [],
       imageIndex: 0,
       currentImageUrl: ''
@@ -23,6 +26,7 @@ class App extends React.Component {
       this.setState({
         product: response.data,
         variations: response.data.variations,
+        currentVariant: response.data.variations[0],
         currentVariantsImages: response.data.variations[0].images,
         currentImageUrl: response.data.variations[0].images[this.state.imageIndex].src
       })
@@ -44,26 +48,34 @@ class App extends React.Component {
   }
 
   render() {
-    // var imageUrl = '';
-    // if (this.state.variations[0]) {
-    //   var variant = this.state.variations[0];
-    //   imageUrl = variant.images[0].src;
-    // }
     return(
-      <div>
-        <div>
-          <h4>{this.state.product.title}</h4>
+      <div className={"wrapper"}>
+        <div className="title">
+          <Title
+            product={this.state.product}
+            currentVariant={this.state.currentVariant}
+          />
         </div>
-        <div>
-          <p>{this.state.product.description}</p>
-        </div>
-        <div>
+        <div className="one">
           <Carousel
             images={this.state.currentVariantsImages}
             currentImageUrl={this.state.currentImageUrl}
             imageIndex={this.state.imageIndex}
             handleClick={this.onThumbnailClick.bind(this)}
           />
+        </div>
+        <div className="two">
+          <div className="description">
+            <h5><b>Product Description</b></h5>
+            <p>{this.state.product.description}</p>
+          </div>
+          <div>
+          <h4>In Stock & Ready to Ship</h4>
+          <p>Most orders placed before noon ET ship same day (except weekends and holidays).</p>
+          </div>
+        </div>
+        <div className="three">
+
         </div>
       </div>
     )
---
+++ client/Title.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import StarRatings from 'react-star-ratings';
+
+function Title(props) {
+  return (
+    <div>
+      <h1>{props.product.title + ' ' + props.currentVariant.color}</h1>
+      <span>
+        {'Item #: ' + props.product._id}
+        <StarRatings
+          rating={4}
+          numberOfStars={5}
+          starRatedColor="gold"
+          starEmptyColor="lightgray"
+          starDimension="15px"
+          starSpacing="0.25px"
+          name='rating'/>
+          9 Reviews | Write a Review | Questions & Answers
+      </span>
+    </div>
+  );
+}
+
+export default Title;
