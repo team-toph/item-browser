cdd84cd6c4a439c1dc52dfe3d4cd0f7f55bfdb7f
xApnea <jcbruno150@gmail.com>
2020-06-26 18:16:02 -0400

    Added Add To Cart Button

--- client/Cart.jsx
+++ client/Cart.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import QuantitySelector from './QuantitySelector.jsx';
 
 const CartBox = styled.div`
   background-color: whitesmoke;
@@ -42,6 +43,24 @@ const CardImageContainer = styled.div`
   margin: 2px 10px 0 0;
 `
 
+const AddToCart = styled.button`
+  background: #cd2418;
+  :hover {
+    background: #c94739;
+  }
+
+  border-radius: 4px;
+  border: 1px solid whitesmoke;
+  border-width: 0 0 1px 1px;
+  border-top: 1px solid #f3f4f5;
+  border-right: 1px solid #e6e8e8;
+  font-family: 'Montserrat', sans-serif;
+  font-size: 18px;
+  height: 42px;
+  width: 200px;
+  color: whitesmoke;
+`;
+
 function Cart(props) {
   return (
     <CartBox>
@@ -57,6 +76,10 @@ function Cart(props) {
             Special 6-month financing^ + <u>$34{"\n"} back in rewards.</u> Valid through{"\n"} 12/31/2020. <u>Get Details</u>
           </FinancingBlurb>
         </span>
+      </div>
+      <div>
+        <QuantitySelector/>
+        <AddToCart>Add To Cart</AddToCart>
       </div>
     </CartBox>
   );
