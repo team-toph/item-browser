1bb213b3792c7322d2592d11062b45a445e44887
xApnea <jcbruno150@gmail.com>
2020-06-26 18:16:23 -0400

    Added on hover styling to review text, whitespace, and minor styling

--- client/Title.jsx
+++ client/Title.jsx
@@ -1,22 +1,47 @@
 import React from 'react';
 import StarRatings from 'react-star-ratings';
+import styled from 'styled-components';
+
+const Text = styled.span`
+  color: #333;
+  :hover {
+    color: #c94739;
+  }
+`;
+
+const ItemNum = styled.span`
+  color: #333;
+`;
 
 function Title(props) {
   return (
     <div>
       <h1>{props.product.title + ' ' + props.currentVariant.color}</h1>
-      <span>
+      <ItemNum>
         {'Item #: ' + props.product._id}
-        <StarRatings
-          rating={props.rating}
-          numberOfStars={5}
-          starRatedColor="gold"
-          starEmptyColor="lightgray"
-          starDimension="15px"
-          starSpacing="0.25px"
-          name='rating'/>
-          9 Reviews | Write a Review | Questions & Answers
-      </span>
+        <span>
+          &nbsp;&nbsp;&nbsp;&nbsp;
+          <StarRatings
+            rating={props.rating}
+            numberOfStars={5}
+            starRatedColor="gold"
+            starEmptyColor="lightgray"
+            starDimension="15px"
+            starSpacing="0.25px"
+            name='rating'/>
+        </span>
+        <Text>
+            &nbsp;&nbsp;&nbsp;&nbsp;<u>9 Reviews</u>
+        </Text>
+          <span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
+        <Text>
+            <u>Write a Review</u>
+        </Text>
+          <span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
+        <Text>
+            <u>Questions & Answers</u>
+        </Text>
+      </ItemNum>
     </div>
   );
 }
